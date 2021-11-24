f89d7f8e363df6a04b3cd29632ef2c9430ea2106
Luke Rhoads <pumpkineater661@gmail.com>
2021-11-24 00:08:13 -0600

    Initial commit

---
+++ command.go
@@ -0,0 +1,13 @@
+package plugintypes
+
+import "github.com/spf13/cobra"
+
+type Command interface {
+	ParentCommand() []string
+	Name() string
+	Usage() string
+	ShortDesc() string
+	LongDesc() string
+	NumArgs() int
+	Exec(*cobra.Command, []string) error
+}
---
+++ hook.go
@@ -0,0 +1,13 @@
+package plugintypes
+
+import "github.com/spf13/cobra"
+
+type Hook interface {
+	ParentCommand() []string
+	Name() string
+	Type() string
+	ShortDesc() string
+
+	PreRun(cmd *cobra.Command, args []string) error
+	PostRun(cmd *cobra.Command, args []string) error
+}
