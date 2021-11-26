d990966f06b3fea5ecc37b383aa7b9a80f06c359
lukerhoads <pumpkineater661@gmail.com>
2021-11-26 14:50:54 -0600

    Readded getter functions to commandmodule interface

--- command.go
+++ command.go
@@ -55,13 +55,79 @@ type Command struct {
 	NumArgs       int
 }
 
-// need rpc functions for this
+// need getter method for the parameters!
 type CommandModule interface {
+	ParentCommand() []string
+	Name() string
+	Usage() string
+	ShortDesc() string
+	LongDesc() string
+	NumArgs() int
 	Exec(*cobra.Command, []string) error
 }
 
 type CommandModuleRPC struct {
 	client *rpc.Client
+}
+
+func (c *CommandModuleRPC) ParentCommand() []string {
+	var commands []string
+	cErr := c.client.Call("Plugin.ParentCommand", new(interface{}), &commands)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return commands
+}
+
+func (c *CommandModuleRPC) Name() string {
+	var name string
+	cErr := c.client.Call("Plugin.Name", new(interface{}), &name)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return name
+}
+
+func (c *CommandModuleRPC) Usage() string {
+	var usage string
+	cErr := c.client.Call("Plugin.Usage", new(interface{}), &usage)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return usage
+}
+
+func (c *CommandModuleRPC) ShortDesc() string {
+	var desc string
+	cErr := c.client.Call("Plugin.ShortDesc", new(interface{}), &desc)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return desc
+}
+
+func (c *CommandModuleRPC) LongDesc() string {
+	var desc string
+	cErr := c.client.Call("Plugin.LongDesc", new(interface{}), &desc)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return desc
+}
+
+func (c *CommandModuleRPC) NumArgs() int {
+	var numArgs int
+	cErr := c.client.Call("Plugin.NumArgs", new(interface{}), &numArgs)
+	if cErr != nil {
+		panic(cErr)
+	}
+
+	return numArgs
 }
 
 func (c *CommandModuleRPC) Exec(cmd *cobra.Command, args []string) error {
@@ -76,6 +142,36 @@ func (c *CommandModuleRPC) Exec(cmd *cobra.Command, args []string) error {
 
 type CommandModuleRPCServer struct {
 	Impl CommandModule
+}
+
+func (h *CommandModuleRPCServer) ParentCommand(args interface{}, resp *[]string) error {
+	*resp = h.Impl.ParentCommand()
+	return nil
+}
+
+func (h *CommandModuleRPCServer) Name(args interface{}, resp *string) error {
+	*resp = h.Impl.Name()
+	return nil
+}
+
+func (h *CommandModuleRPCServer) Usage(args interface{}, resp *string) error {
+	*resp = h.Impl.Usage()
+	return nil
+}
+
+func (h *CommandModuleRPCServer) ShortDesc(args interface{}, resp *string) error {
+	*resp = h.Impl.ShortDesc()
+	return nil
+}
+
+func (h *CommandModuleRPCServer) LongDesc(args interface{}, resp *string) error {
+	*resp = h.Impl.LongDesc()
+	return nil
+}
+
+func (h *CommandModuleRPCServer) NumArgs(args interface{}, resp *int) error {
+	*resp = h.Impl.NumArgs()
+	return nil
 }
 
 func (h *CommandModuleRPCServer) Exec(args ExecArgs, resp *error) error {
